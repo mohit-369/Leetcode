7a021a65d775ab2c5559b64daa40e293ed31c07a
Mohit Kumar Kumawat <124484226+mohit-369@users.noreply.github.com>
2024-03-26 21:15:12 +0530

    Time: 9 ms (69.52%) | Memory: 14.1 MB (17.04%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        int n=nums.size();
+
+        unordered_map<int,int> mpp;
+
+        for(int i=0;i<n;i++)
+        {
+            int complement=target-nums[i];
+
+            if(mpp.count(complement))
+            {
+                return {mpp[complement],i};
+            }
+
+            mpp[nums[i]]=i;
+        }
+
+        return {};
+
+
+
+        
+    }
+};
