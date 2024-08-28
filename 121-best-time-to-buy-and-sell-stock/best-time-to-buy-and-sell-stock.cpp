a4f63684c23614a1240d7a2fc9ca7c210395ec75
Mohit Kumar Kumawat <124484226+mohit-369@users.noreply.github.com>
2024-08-28 07:57:08 +0000

    Time: 259 ms (5.84%) | Memory: 165.8 MB (5.00%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,28 +1,43 @@
 class Solution {
-int solve(vector<int> &prices,int index,int &ans)
+int solve(int index,vector<int> &prices,int sell,vector<vector<int>> &dp)
 {
-    if(index == 0) return prices[0];
 
-    int mi = solve(prices,index-1,ans);
+    if(index==0 && sell)
+    {
+        return INT_MIN;
 
-    ans = max(ans,prices[index]-mi);
+    }
 
-    return min(prices[index],mi);
+    if(index==0 && !sell)
+    {
+        return prices[index];
+    }
+
+    if(dp[index][sell]!=-1) return dp[index][sell];
 
 
 
+    int notsale = solve(index-1,prices,sell,dp);
 
+    int sale=INT_MIN;
+
+    if(sell)
+    {
+        sale = prices[index]-solve(index-1,prices,0,dp);
+
+        return dp[index][sell]= max(notsale,sale);
+    }
+
+    return dp[index][sell]=min(prices[index],notsale);
 }
 public:
     int maxProfit(vector<int>& prices) {
 
-        int ans =INT_MIN;
+        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
 
-        solve(prices,prices.size()-1,ans);
+        int ans = solve(prices.size()-1,prices,1,dp);
 
         return ans<0?0:ans;
-
-        
         
     }
 };
