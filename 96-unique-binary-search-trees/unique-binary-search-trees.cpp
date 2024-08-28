5315272dd8d42178e768f5b49d0027c1b4da84c2
Mohit Kumar Kumawat <124484226+mohit-369@users.noreply.github.com>
2024-08-28 11:44:21 +0000

    Time: 2010 ms (15.82%) | Memory: 7.2 MB (76.77%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numTrees(int n) {
+
+
+        if(n<=1) return 1;
+
+        int result = 0;
+
+        for(int i = 1; i<=n; i++)
+        {
+            result+=numTrees(i-1)*numTrees(n-i);
+        }
+
+        return result;
+        
+    }
+};
