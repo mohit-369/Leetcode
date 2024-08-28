651a669bcfe1c45270cdbd717a9e9c3b3cea9b40
Mohit Kumar Kumawat <124484226+mohit-369@users.noreply.github.com>
2024-08-28 05:17:25 +0000

    Time: 11 ms (37.12%) | Memory: 12.9 MB (54.74%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -22,13 +22,42 @@ public:
     int minDistance(string word1, string word2) {
 
 
-        int id1 = word1.size()-1;
+        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,0));
 
-        int id2 = word2.size()-1;
+        // if s1 =0;
 
-        vector<vector<int>> dp(word1.size(),vector<int>(word2.size(),-1));
+        for(int col = 1; col <= word2.size(); col++)
+        {
+            dp[0][col]=col;
+        
+        }
 
-        return solve(id1,id2,word1,word2,dp);
+        for(int row = 1; row <= word1.size(); row++)
+        {
+            dp[row][0]=row;
+        }
+
+
+
+        for(int i = 1; i <= word1.size(); i++)
+        {
+            for(int j = 1; j <= word2.size(); j++)
+            {
+                if(word1[i-1] == word2[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+
+                else
+                {
+                    dp[i][j] = 1 + min({dp[i-1][j-1],dp[i][j-1],dp[i-1][j]});
+                }
+            }
+        }
+
+        return dp[word1.size()][word2.size()];
+
+        
         
     }
 };
