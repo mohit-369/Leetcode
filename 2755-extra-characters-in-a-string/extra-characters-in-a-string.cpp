e0c809e1b545aaa53fb47a83d2eca09843618051
Mohit Kumar Kumawat <124484226+mohit-369@users.noreply.github.com>
2024-09-23 06:50:11 +0000

    Time: 680 ms (5.07%) | Memory: 317.8 MB (5.07%) - LeetSync

---
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+int solve(string s, int index,unordered_set<string> st,unordered_map<int,int> &dp)
+{
+    if(index >= s.size()) return 0;
+
+    if(dp.find(index) != dp.end()) return dp[index];
+
+    int ans = INT_MAX;
+
+    string temp = "";
+
+    for(int i = index ; i < s.size(); i++)
+    {
+        temp+=s[i];
+
+        if(st.find(temp) != st.end())
+        {
+            ans = min(ans,solve(s,i+1,st,dp));
+        }
+    }
+
+    ans = min(ans, 1 + solve(s,index+1,st,dp));
+
+    return dp[index] = ans;
+}
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+
+        unordered_map<int,int> dp;
+
+        unordered_set<string> st(dictionary.begin(),dictionary.end());
+
+        return solve(s,0,st,dp);
+        
+    }
+};
