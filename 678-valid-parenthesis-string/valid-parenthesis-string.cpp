82f05e16fe9f6bfa2a2c57ddb159f2b263d6a12c
Mohit Kumar Kumawat <124484226+mohit-369@users.noreply.github.com>
2024-10-08 05:21:30 +0000

    Time: 0 ms (100.00%) | Memory: 9.8 MB (11.73%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,17 +1,51 @@
 class Solution {
+bool solve(int idx, int count,string &s,vector<vector<int>> &dp)
+{
+
+    if(idx == s.size())
+    {
+        return count == 0;
+    }
+
+    if(dp[idx][count] != -1) return dp[idx][count];
+
+
+    if(s[idx] == '(')
+    {
+        return dp[idx][count] = solve(idx+1,count + 1,s,dp);
+    }
+
+    else if(s[idx] == ')')
+    {
+        if(count)
+        {
+           return dp[idx][count] = solve(idx+1,count - 1,s,dp);
+        }
+
+        return 0;
+    }
+
+    int a = solve(idx + 1, count, s,dp);
+
+    int b = solve(idx + 1, count + 1, s,dp);
+
+    int c = 0;
+
+    if(count)
+    {
+        c = solve(idx +1, count - 1, s,dp);
+    }
+
+    return dp[idx][count] = a || b || c;
+
+
+}
 public:
     bool checkValidString(string s) {
-        int cmin = 0, cmax = 0;
-        for (char c : s) {
-            if (c == '(')
-                cmax++, cmin++;
-            if (c == ')')
-                cmax--, cmin = max(cmin - 1, 0);
-            if (c == '*')
-                cmax++, cmin = max(cmin - 1, 0);
-            if (cmax < 0)
-                return false;
-        }
-        return cmin == 0;
+
+        vector<vector<int>> dp(s.size() + 1,vector<int> (s.size(),-1));
+
+        return solve(0,0,s,dp);
+        
     }
 };
