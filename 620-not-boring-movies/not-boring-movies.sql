3507e0558b38fe2989dfe95bf2c6b137d000dbd1
Mohit Kumar Kumawat <124484226+mohit-369@users.noreply.github.com>
2024-12-27 17:57:53 +0000

    Time: 274 ms (47.72%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -2,3 +2,8 @@ select *
 from Cinema
 where id % 2 != 0 and description != 'boring'
 order by rating desc;
+
+
+-- SELECT * FROM Cinema WHERE MOD( id, 2) = 1 AND 
+
+-- description <> 'boring' ORDER BY rating DESC
