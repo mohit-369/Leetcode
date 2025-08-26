3593c6cffc6628bc18770c3c4262951a0a849538
Mohit Kumar Kumawat <124484226+mohit-369@users.noreply.github.com>
2025-08-26 19:28:02 +0000

    Time: 259 ms (26.33%) | Memory: 63.7 MB (25.65%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,55 +1,44 @@
 class Solution {
-void solve(string s, set<string> &st, int &ans)
-{
-    if(s.size() == 0)
+
+public:
+    void solve(int index, set<string> &st, string &s, int &ans)
     {
-        int x = st.size();
-        ans = max(ans,x);
+        if(index == s.size())
+        {
+            int size = st.size();
+            ans = max(ans,size);
+            // for(auto &it:st)
+            // {
+            //     cout<<it<<" "<<endl;
+            // }
+            // cout<<st.size()<<endl;
+
+            return;
+        }
+
+        string temp;
+
+        for(int i = index; i < s.size(); i++)
+        {
+            temp.push_back(s[i]);
+            if(st.find(temp) == st.end())
+            {
+                st.insert(temp);
+            solve(i+1,st,s,ans);
+            st.erase(temp);
+            }
+            
+        }
 
         return ;
     }
-
-    if(s.size() == 1)
-    {
-        if(st.count(s) == 0)
-        {
-            int x = st.size();
-            ans = max(ans , 1 + x);
-        }
-
-        else
-        {
-            int x = st.size();
-            ans = max(ans, x);
-        }
-
-        return;
-    }
-
-    for(int i = 0; i < s.size(); i++)
-    {
-        string temp = s.substr(0, i + 1);
-
-        if(st.count(temp) == 0)
-        {
-            st.insert(temp);
-
-            solve(s.substr(i + 1), st, ans);
-
-            st.erase(temp);
-        }
-    }
-
-    return ;
-}
-public:
     int maxUniqueSplit(string s) {
-
-        int ans = 1;
 
         set<string> st;
 
-        solve(s,st,ans);
+        int ans = 1;
+
+        solve(0,st,s,ans);
 
         return ans;
         
